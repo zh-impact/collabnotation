5faf4d6b332e39356453706e81d5d02b27fa68d1
Zhi.Huang <zh.impact@outlook.com>
2025-04-08 23:46:13 +0800

    update: basic ui

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import bundleAnalyzer from "@next/bundle-analyzer";
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+  },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
